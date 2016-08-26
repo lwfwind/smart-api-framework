aa607d789cb867c90b145888f766f34a5e15270d
ZhangyuBaolu <1196230036@qq.com>
2016-08-26 15:50:56 +0800

    test

--- src/com/qa/framework/core/ParamValueProcessor.java
+++ src/com/qa/framework/core/ParamValueProcessor.java
@@ -127,6 +127,7 @@ public class ParamValueProcessor {
     }
 
     private void processParamDate(Param param, Setup setup, TestData testData) {
+        logger.info(param.getDateStamp()+"---"+testData.getDesc());
         if (param.getDateStamp() != null) {
             DateStamp dateStamp = param.getDateStamp();
             Calendar c = Calendar.getInstance();
