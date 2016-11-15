2738aceed1da59b7ed0dd3efdf0b7d2f2804028e
ZhangyuBaolu <1196230036@qq.com>
2016-11-15 11:32:12 +0800

    update sendMessage

--- src/com/qa/framework/library/sms/sendMessage.java
+++ src/com/qa/framework/library/sms/sendMessage.java
@@ -1,7 +1,9 @@
 package com.qa.framework.library.sms;
 
+import com.qa.framework.bean.Param;
 import com.qa.framework.config.PropConfig;
 import com.qa.framework.library.httpclient.HttpConnectionImp;
+import com.qa.framework.library.httpclient.HttpMethod;
 import org.apache.http.HttpHost;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -16,43 +18,39 @@ import java.util.*;
  * Created by Administrator on 2016/9/21.
  */
 public class sendMessage {
-    private static boolean useProxy = PropConfig.isUseProxy();
-
     public static String sendMsg(String mobile,String message) throws  IOException {
-        HttpPost httpPost = new HttpPost("http://sdk2.entinfo.cn/webservice.asmx/SendSMS");
-        RequestConfig requestConfig;
-        int timeout=50000;
-        if (useProxy) {
-            HttpHost proxy = new HttpHost("127.0.0.1", 8888, "http");
-            requestConfig = RequestConfig.custom().setSocketTimeout(timeout).setConnectTimeout(timeout).setProxy(proxy).build();
-        } else {
-            requestConfig = RequestConfig.custom().setSocketTimeout(timeout).setConnectTimeout(timeout).build();
-        }
-        httpPost.setConfig(requestConfig);
-        Map<String,String> params=new LinkedHashMap<String,String>();
-        params.put("sn",PropConfig.getSN());
-        params.put("pwd",PropConfig.getSNPWD());
-        params.put("mobile",mobile);
-        params.put("content",message);
-        Set<String> keys=params.keySet();
-        List<BasicNameValuePair> basicNameValuePairs = new ArrayList<BasicNameValuePair>();
-        for (String key : keys) {
-            BasicNameValuePair basicNameValuePair = new BasicNameValuePair(key, params.get(key));
-            basicNameValuePairs.add(basicNameValuePair);
-        }
-        try {
-            httpPost.setEntity(new UrlEncodedFormEntity(basicNameValuePairs, "UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        HttpConnectionImp imp = new HttpConnectionImp(httpPost);
-        String result=imp.getResponseResult(false,false);
+        String url="http://sdk2.entinfo.cn/webservice.asmx/SendSMS";
+
+        List<Param> params=addParams(mobile,message);
+        String result= HttpMethod.usePostMethod(url,params,false,false);
         if (result.contains("成功")){
             return mobile+"发送短信成功";
         }else{
             return mobile+"发送短信失败，错误代码为:"+result;
         }
     }
+
+    private static List<Param> addParams(String mobile, String message) {
+        List<Param> params=new ArrayList<Param>();
+        Param param1=new Param();
+        param1.setName("sn");
+        param1.setValue(PropConfig.getSN());
+        Param param2=new Param();
+        param2.setName("pwd");
+        param2.setValue(PropConfig.getSNPWD());
+        Param param3=new Param();
+        param3.setName("mobile");
+        param3.setValue(mobile);
+        Param param4=new Param();
+        param4.setName("content");
+        param4.setValue(message);
+        params.add(param1);
+        params.add(param2);
+        params.add(param3);
+        params.add(param4);
+        return params;
+    }
+
     public static String sendMsg(List<String> mobiles,String message) throws IOException {
         String afterSend="短信结果：\n";
         for(String mobile:mobiles){
