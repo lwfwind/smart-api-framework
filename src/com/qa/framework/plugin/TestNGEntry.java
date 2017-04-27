0225280ed2ebe35c08b346936fb1d60e595b15ac
lwf134206 <lwf134206@alibabapictures.com>
2017-04-27 21:10:52 +0800

    支持运行和调试testng测试

---
+++ src/com/qa/framework/plugin/TestNGEntry.java
@@ -0,0 +1,56 @@
+package com.qa.framework.plugin;
+
+import com.qa.framework.bean.TestCase;
+import com.qa.framework.core.DataManager;
+import com.qa.framework.core.ParamValueProcessor;
+import com.qa.framework.library.httpclient.HttpMethod;
+import com.qa.framework.mock.IMockServer;
+import com.qa.framework.verify.Verify;
+import org.apache.log4j.Logger;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static com.qa.framework.classfinder.ClassHelper.findImplementClass;
+
+public class TestNGEntry {
+    private static Logger logger = Logger.getLogger(Entry.class);
+    private static IMockServer mockServer = null;
+
+
+    @Test(dataProviderClass = DataManager.class, dataProvider = "data")
+    public void debug(TestCase testCase, String url, String httpMethod) {
+        ParamValueProcessor.processTestData(testCase);
+        String content = HttpMethod.request(url, testCase.getHeaders(), testCase.getParams(), httpMethod, testCase.isStoreCookie(), testCase.isUseCookie());
+        Verify.verifyResult(testCase, content);
+        ParamValueProcessor.processAfter(testCase);
+    }
+
+    @BeforeTest(alwaysRun = true)
+    public void beforeTest() {
+        Class<?> clazz = findImplementClass(IMockServer.class);
+        if (clazz != null) {
+            try {
+                mockServer = (IMockServer) clazz.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                logger.error(e.getMessage(),e);
+            }
+            mockServer.startServer();
+            mockServer.settingRules();
+        }
+        String testSuiteName = System.getProperty("testSuiteName");
+        String testCaseName = System.getProperty("testCaseName");
+        DataManager.setXmlName(testSuiteName);
+        if (!testCaseName.equalsIgnoreCase("null")) {
+            DataManager.setXmlDataName(testCaseName);
+        }
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void afterBefore() {
+        if (mockServer != null) {
+            mockServer.stopServer();
+        }
+    }
+}
